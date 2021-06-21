64392cc6eddab688d6cf6b71be0b647317468975
Zeyuan Dong <snovian@gmail.com>
2021-06-21 14:05:54 +0800

    temp save

---
+++ bigdata/base/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+docker build --force-rm -t snovian/bigdata-base:0.1 -t snovian/bigdata-base:latest .
---
+++ os/debian/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+docker build --force-rm -t snovian/debian:10 -t snovian/debian:latest .
